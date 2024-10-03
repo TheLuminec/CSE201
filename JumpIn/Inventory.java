9e1517fe822fcb14449bcc31a94df1874bf144bc
HeroBrianMack <111471045+HeroBrianMack@users.noreply.github.com>
2024-10-03 15:58:30 -0400

    Inventory

--- JumpIn/Inventory.java
+++ JumpIn/Inventory.java
@@ -2,9 +2,11 @@ import java.util.HashSet;
 
 public class Inventory {
 
+    // Note Change to Item Class when made
     HashSet<String> inventory;
     int size = 3;
     public Inventory() {
+        // Note Change to Item Class when made
         inventory = new HashSet<String>();
     }
 }
