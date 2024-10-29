28dab3e3c0803f5043a622d1a590905eb6dda38b
HeroBrianMack <111471045+HeroBrianMack@users.noreply.github.com>
2024-10-29 16:00:44 -0400

    Driver initial + Class initials

--- Driver.java
+++ Driver.java
@@ -1,4 +1,7 @@
 public class Driver {
+        private static final Room[] rooms;
+        private static final Player player = new Player();
+
     public static void main(String[] args) {
         
     }
