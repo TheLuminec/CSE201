bddbf8d072db67f08fdce34c6e2ee49d198bc24e
HeroBrianMack <111471045+HeroBrianMack@users.noreply.github.com>
2024-10-29 16:06:03 -0400

    Driver basic setup w/username

--- Driver.java
+++ Driver.java
@@ -1,12 +1,20 @@
 
 import java.util.ArrayList;
-
+import java.util.Scanner;
 public class Driver {
         private static ArrayList<Room> rooms;
-        private static Player player = new Player();
+        private static Player player;
+        // put something here
+        private static final Room startRoom = null;
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String userName = in.next();
+        player = new Player(userName, startRoom, null);
         
-        
+    }
+
+    public void makeStory() {
+
     }
 }
