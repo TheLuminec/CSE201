f97c6b7346c861957f1d4e4a6428383237a207c9
HeroBrianMack <111471045+HeroBrianMack@users.noreply.github.com>
2024-10-29 15:55:05 -0400

    Options Class

--- Option.java
+++ Option.java
@@ -1,1 +1,33 @@
+import java.util.ArrayList;
 
+public class Option {
+    final private ArrayList<String> flagsNeeded;
+    final private ArrayList<String> flagsTriggered;
+    private final String description;
+    private boolean isDone = false;
+
+    public Option(ArrayList<String> flagsNeeded, ArrayList<String> flagsTriggered, String description) {
+        this.flagsNeeded = flagsNeeded;
+        this.flagsTriggered = flagsTriggered;
+        this.description = description;
+    }
+
+    public ArrayList<String> triggerFlags() {
+        isDone = true;
+        return flagsTriggered;
+    }
+
+    public boolean getIsDone(ArrayList<String> flags) {
+        return isDone;
+    }
+
+    public ArrayList<String> getNeededFlags() {
+        return flagsNeeded;
+    }
+
+    @Override
+    public String toString() {
+        return description;
+    }
+
+}
