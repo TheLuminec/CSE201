d97ff947f5e023f9c8236794430ec567358f7a88
valeskjw <valeskjw@miamioh.edu>
2024-10-29 16:02:51 -0400

    Update Player.java

--- Player.java
+++ Player.java
@@ -1,3 +1,27 @@
-public class Player {
+public class Player{
+    private String username;
+    private Room location;
+    private Inventory inventory;
     
+    public Player(String username, Room location, Inventory inventory) {
+        this.username = username;
+        this.location = location;
+        this.inventory = inventory; 
+    }
+    
+    public String getName() {
+        return this.username;
+    }
+    public Room getRoom() {
+        return this.location;
+    }
+    public void moveRoom(Room room) {
+        this.location = room;
+    }
+    public Inventory getInventory() {
+        return this.inventory;
+    }
+    public String getUserName() {
+        return this.username;
+    }
 }
