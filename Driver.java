b990650a5c41c3ccfc6eefbb26b4d6b862a6eb9b
theluminec <theluminec@gmail.com>
2024-10-31 16:17:00 -0400

    Javadocs Comments

--- Driver.java
+++ Driver.java
@@ -3,13 +3,21 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 
+/**
+ * The main driver class for the text adventure game.
+ * It initializes the game, handles user input, and runs the game loop.
+ */
 public class Driver {
     private static ArrayList<Room> rooms;
     private static Player player;
-    // put something here
     private static Room startRoom = null;
     private static Scanner input;
 
+    /**
+     * The main method to start the game.
+     *
+     * @param args Command-line arguments (not used).
+     */
     public static void main(String[] args) {
         input = new Scanner(System.in);
         System.out.println("Enter your Name!");
@@ -24,50 +32,52 @@ public class Driver {
         input.close();
     }
 
+    /**
+     * Runs the main game loop, processing player input and game state.
+     */
     public static void run() {
         player.getRoom().enterRoom();
 
-        while(true) {
+        while (true) {
             Room currRoom = player.getRoom();
-            if(player.timeIsUp()) {
+            if (player.timeIsUp()) {
                 System.out.println("Game Over!");
                 break;
             }
 
             currRoom.printOptions();
             int choice = 0;
-            try{
+            try {
                 choice = input.nextInt();
-            } catch(Exception e) {
+            } catch (Exception e) {
                 System.out.println("Invalid Option! Please enter a number.");
+                input.nextLine(); // Clear invalid input
                 continue;
             }
-            
-            if(currRoom.chooseOption(choice, player)) {
-                //score and things
-                if(currRoom.hasFlag("gameEnd")) {
+
+            if (currRoom.chooseOption(choice, player)) {
+                // Check for game end condition
+                if (currRoom.hasFlag("gameEnd")) {
                     break;
                 }
-            }
-            else {
+            } else {
                 System.out.println("Invalid Option!");
                 continue;
             }
-
         }
-
-        
     }
 
-    private static void makeStory() {//example
+    /**
+     * Sets up the game story by creating rooms and their respective options.
+     */
+    private static void makeStory() {
         rooms = new ArrayList<>();
-        Room bedroom = new Room("Bedroom", "there are things in here, bedroom");
-        Room closet = new Room("Closet", "closet (hammer)");
-        Room hallway = new Room("Hallway", "hallway stuff");
-        Room storage = new Room("Storage", "storage stuff");
-        Room purifier = new Room("Purifier", "purifier stuff");
-        Room controlRoom = new Room("Control Room", "control stuff");
-
+        Room bedroom = new Room("Bedroom", "There are things in here, bedroom.");
+        Room closet = new Room("Closet", "Closet (hammer)");
+        Room hallway = new Room("Hallway", "Hallway stuff");
+        Room storage = new Room("Storage", "Storage stuff");
+        Room purifier = new Room("Purifier", "Purifier stuff");
+        Room controlRoom = new Room("Control Room", "Control stuff");
 
         rooms.add(bedroom);
         rooms.add(closet);
@@ -76,54 +86,73 @@ public class Driver {
         rooms.add(purifier);
         rooms.add(controlRoom);
 
-
         bedroomStory(bedroom);
         closetStory(closet);
         hallwayStory(hallway);
         purifierStory(purifier);
 
-    
-
         startRoom = bedroom;
     }
 
+    /**
+     * Defines the options and flags for the bedroom.
+     *
+     * @param bedroom The bedroom room object.
+     */
     private static void bedroomStory(Room bedroom) {
         bedroom.addOption(new Option(null, flags("hasMissile"), "missile: option 1", "can pick up missile"));
         bedroom.addOption(new Option(null, null, "closet: option 2", "go to closet"));
         bedroom.addOption(new Option(null, flags("alarmVisit"), "clock: option 3", "go to clock"));
-        bedroom.addOption(new Option(null, null, "leave: option 4", "leave hallway"));
-        bedroom.addOption(new Option(flags("alarmVisit", "hasHammer"),
-         flags("alarmbreak"), "clock: option 2", "go to clock", rooms.get(1)));
+        bedroom.addOption(new Option(null, null, "leave: option 4", "leave to hallway"));
+        bedroom.addOption(new Option(
+                flags("alarmVisit", "hasHammer"),
+                flags("alarmbreak"),
+                "clock: option 2",
+                "go to clock",
+                rooms.get(1)
+        ));
     }
 
+    /**
+     * Defines the options and flags for the closet.
+     *
+     * @param closet The closet room object.
+     */
     private static void closetStory(Room closet) {
         closet.addOption(new Option(null, null, "leave: option 1", "leave closet"));
         closet.addOption(new Option(null, null, "clothes: option 2", "clothes"));
     }
-    
+
+    /**
+     * Defines the options and flags for the hallway.
+     *
+     * @param hallway The hallway room object.
+     */
     private static void hallwayStory(Room hallway) {
-        hallway.addOption(new Option(null, null, "bedroom leave: option 1 ", "go bedroom", rooms.get(0)));
-        hallway.addOption(new Option(null, null, "broken storage: option 2 ", "see Storage", rooms.get(3)));
-        hallway.addOption(new Option(null, null, "purifier leave: option 3 ", "go purifier", rooms.get(4)));
-        hallway.addOption(new Option(null, null, "control room leave: option 4 ", "go control room", rooms.get(5)));
+        hallway.addOption(new Option(null, null, "bedroom leave: option 1", "go to bedroom", rooms.get(0)));
+        hallway.addOption(new Option(null, null, "broken storage: option 2", "see Storage", rooms.get(3)));
+        hallway.addOption(new Option(null, null, "purifier leave: option 3", "go to purifier", rooms.get(4)));
+        hallway.addOption(new Option(null, null, "control room leave: option 4", "go to control room", rooms.get(5)));
     }
 
-    private static void storageStory(Room storage) {
-        storage.addOption(new Option(flags("hasMissile"), flags("gameEnd"), "missile: option 1", "Death by missile"));
-        storage.addOption(new Option(flags("hasHammer"), flags("openRoom"), "hammer: option 2", "Death by missile"));
-        storage.addOption(new Option(flags("openRoom"), flags("openRoom"), "hammer: option 2", "Death by missile"));
-
-
-    }
-
+    /**
+     * Defines the options and flags for the purifier room.
+     *
+     * @param purifier The purifier room object.
+     */
     private static void purifierStory(Room purifier) {
         purifier.addOption(new Option(null, null, "control panel: option 1", "control info"));
         purifier.addOption(new Option(null, flags("hasFilter"), "filter panel: option 2", "filter gain"));
-        purifier.addOption(new Option(null, null, "leave: option 3", "leave control", rooms.get(2)));
+        purifier.addOption(new Option(null, null, "leave: option 3", "leave purifier", rooms.get(2)));
     }
 
+    /**
+     * Helper method to create a list of flags.
+     *
+     * @param flag One or more flag strings.
+     * @return A list of flags.
+     */
     private static List<String> flags(String... flag) {
         return Arrays.asList(flag);
     }
-
 }
--- Option.java
+++ Option.java
@@ -1,22 +1,37 @@
 import java.util.ArrayList;
 import java.util.Collection;
 
+/**
+ * Represents an actionable option within a room.
+ * Options may have prerequisites and can trigger certain game flags.
+ */
 public class Option {
-    final private ArrayList<String> flagsNeeded;
-    final private ArrayList<String> flagsTriggered;
+    private final ArrayList<String> flagsNeeded;
+    private final ArrayList<String> flagsTriggered;
     private final String description;
     private final String result;
     private boolean isDone = false;
     private int turnCost;
     private Room toRoom;
 
-    public Option(Collection<String> flagsNeeded, Collection<String> flagsTriggered, String description, String result, int turnCost, Room toRoom) {
-        if(flagsNeeded != null)
+    /**
+     * Constructs an Option with specified parameters.
+     *
+     * @param flagsNeeded    Flags required for this option to be available.
+     * @param flagsTriggered Flags that will be triggered upon choosing this option.
+     * @param description    The description of the option.
+     * @param result         The result text displayed after choosing the option.
+     * @param turnCost       The number of turns this option consumes.
+     * @param toRoom         The room to move to after choosing this option (if any).
+     */
+    public Option(Collection<String> flagsNeeded, Collection<String> flagsTriggered,
+                  String description, String result, int turnCost, Room toRoom) {
+        if (flagsNeeded != null)
             this.flagsNeeded = new ArrayList<>(flagsNeeded);
         else
             this.flagsNeeded = new ArrayList<>();
 
-        if(flagsTriggered != null)
+        if (flagsTriggered != null)
             this.flagsTriggered = new ArrayList<>(flagsTriggered);
         else
             this.flagsTriggered = new ArrayList<>();
@@ -27,39 +42,86 @@ public class Option {
         this.toRoom = toRoom;
     }
 
-
-
-    public Option(Collection<String> flagsNeeded, Collection<String> flagsTriggered, String description, String result) {
+    /**
+     * Constructs an Option with default turn cost and no room change.
+     *
+     * @param flagsNeeded    Flags required for this option to be available.
+     * @param flagsTriggered Flags that will be triggered upon choosing this option.
+     * @param description    The description of the option.
+     * @param result         The result text displayed after choosing the option.
+     */
+    public Option(Collection<String> flagsNeeded, Collection<String> flagsTriggered,
+                  String description, String result) {
         this(flagsNeeded, flagsTriggered, description, result, 1, null);
     }
 
-    public Option(Collection<String> flagsNeeded, Collection<String> flagsTriggered, String description, String result, Room room) {
+    /**
+     * Constructs an Option that leads to another room.
+     *
+     * @param flagsNeeded    Flags required for this option to be available.
+     * @param flagsTriggered Flags that will be triggered upon choosing this option.
+     * @param description    The description of the option.
+     * @param result         The result text displayed after choosing the option.
+     * @param room           The room to move to after choosing this option.
+     */
+    public Option(Collection<String> flagsNeeded, Collection<String> flagsTriggered,
+                  String description, String result, Room room) {
         this(flagsNeeded, flagsTriggered, description, result, 1, room);
     }
 
+    /**
+     * Triggers the flags associated with this option.
+     *
+     * @return The list of flags that were triggered.
+     */
     public ArrayList<String> triggerFlags() {
         isDone = true;
         return flagsTriggered;
     }
 
+    /**
+     * Checks if this option has already been completed.
+     *
+     * @return True if the option is done, false otherwise.
+     */
     public boolean getIsDone() {
-        if(toRoom != null)
+        if (toRoom != null)
             return false;
         return isDone;
     }
 
+    /**
+     * Gets the flags required to make this option available.
+     *
+     * @return A list of required flags.
+     */
     public ArrayList<String> getNeededFlags() {
         return flagsNeeded;
     }
 
+    /**
+     * Gets the result text to display after choosing this option.
+     *
+     * @return The result string.
+     */
     public String getResult() {
         return result;
     }
 
+    /**
+     * Gets the turn cost of this option.
+     *
+     * @return The number of turns this option consumes.
+     */
     public int getTurnCost() {
         return turnCost;
     }
 
+    /**
+     * Gets the room to move to after choosing this option.
+     *
+     * @return The room object, or null if there is no room change.
+     */
     public Room getToRoom() {
         return toRoom;
     }
@@ -68,5 +130,4 @@ public class Option {
     public String toString() {
         return description;
     }
-
 }
