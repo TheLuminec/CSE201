e543591bf0ae19f12e5cf8bd665e629140f6c0fb
theluminec <theluminec@gmail.com>
2024-11-19 15:58:31 -0500

    tmp

--- Driver.java
+++ Driver.java
@@ -92,13 +92,22 @@ public class Driver {
      */
     private static void makeStory() {//example
         rooms = new ArrayList<>();
-        Room bedroom = new Room("Bedroom", "there are things in here, bedroom");
+        Room bedroom = new Room("Bedroom", 
+        "You wake up on your spaceship to a blaring alarm repeating
+        \“the air purification system is damaged. Proceed to the purification room to do further diagnostics.\”.   
+        You look around the room and see [1] a door straight ahead,  [2] a closet to the right, [3] an alarm clock to the left, 
+        and [4] an old missile on the wall");
+        bedroom.addOption(new Option("You inspect the missile and notice that it is still live and functional, would you like to take it?"
+        , "You take the missile somehow carrying it."));
+        
+
+
+
         Room closet = new Room("Closet", "closet (hammer)");
         Room hallway = new Room("Hallway", "hallway stuff");
         Room storage = new Room("Storage", "storage stuff");
         Room purifier = new Room("Purifier", "purifier stuff");
         Room controlRoom = new Room("Control Room", "control stuff");
-
 
         rooms.add(bedroom);
         rooms.add(closet);
@@ -106,89 +115,7 @@ public class Driver {
         rooms.add(storage);
         rooms.add(purifier);
         rooms.add(controlRoom);
-
-
-        bedroomStory(bedroom);
-        closetStory(closet);
-        hallwayStory(hallway);
-        storageStory(storage);
-        purifierStory(purifier);
-        controlRoom(controlRoom);
-    
-
         startRoom = bedroom;
-    }
-
-    /**
-     * Defines the options and flags for the bedroom.
-     *
-     * @param bedroom The bedroom room object.
-     */
-    private static void bedroomStory(Room bedroom) {
-        // bedroom.addOption(new Option(null, flags("hasMissile"), "missile", "can pick up missile"));
-        // bedroom.addOption(new Option(null, null, "closet", "go to closet"));
-        // bedroom.addOption(new Option(null, flags("alarmVisit"), "clock", "go to clock"));
-        // bedroom.addOption(new Option(flags("alarmVisit"), flags("hasBatteries"), "break clock", "break clock + battery"));
-        // bedroom.addOption(new Option(null, null, "leave", "leave hallway", rooms.get(2)));
-
-    }
-
-    /**
-     * Defines the options and flags for the closet.
-     *
-     * @param closet The closet room object.
-     */
-    private static void closetStory(Room closet) {
-        // closet.addOption(new Option(null, null, "leave", "leave closet", rooms.get(0)));
-        // closet.addOption(new Option(null, flags("hasHammer"), "hammer", "hammer get"));
-        // closet.addOption(new Option(null, null, "clothes", "clothes"));
-    }
-    
-    /**
-     * Defines the options and flags for the hallway.
-     *
-     * @param hallway The hallway room object.
-     */
-    private static void hallwayStory(Room hallway) {
-        // hallway.addOption(new Option(null, null, "bedroom leave", "go bedroom", rooms.get(0)));
-        // hallway.addOption(new Option(null, null, "broken storage", "see Storage", rooms.get(3)));
-        // hallway.addOption(new Option(null, null, "purifier leave", "go purifier", rooms.get(4)));
-        // hallway.addOption(new Option(null, null, "control room leave", "go control room", rooms.get(5)));
-    }
-
-    /**
-     * Defines the options and flags for the storage room.
-     *
-     * @param purifier The storage room object.
-     */ 
-    private static void storageStory(Room storage) {
-        // storage.addOption(new Option(flags("hasMissile"), flags("gameEnd"), "missile", "Death by missile"));
-        // storage.addOption(new Option(flags("hasBatteries"), flags("openRoom"), "hammer", "Door open"));
-        // storage.addOption(new Option(null, null, "leave", "leave room", rooms.get(2)));
-    }
-
-    /**
-     * Defines the options and flags for the purifier room.
-     *
-     * @param purifier The purifier room object.
-     */ 
-    private static void purifierStory(Room purifier) {
-        // purifier.addOption(new Option(null, null, "control panel", "control info"));
-        // purifier.addOption(new Option(null, flags("gameEnd"), "fix air purifier", "Congrats! You fixed the air purifier!"));
-        // purifier.addOption(new Option(null, null, "filter panel", "filter gain"));
-        // purifier.addOption(new Option(null, null, "leave", "leave purifier", rooms.get(2)));
-    }
-
-    /**
-     * Defines the options and flags for the control room.
-     *
-     * @param purifier The control room object.
-     */ 
-    private static void controlRoom(Room controlRoom) {
-        // controlRoom.addOption(new Option(null, null, "view console", "console info"));
-        // controlRoom.addOption(new Option(null, flags("hasCard"), "key card", "keycard grab"));
-        // controlRoom.addOption(new Option(null, flags("hasFilter"), "filter gain", "filter gain"));
-        // controlRoom.addOption(new Option(null, null, "leave", "leave control room", rooms.get(2)));
 
 
     }
