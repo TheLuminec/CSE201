9ff1a01eb8e8c5c199f0e62c6d39011f222015ac
theluminec <theluminec@gmail.com>
2024-11-21 15:52:21 -0500

    Story prolly works.

--- Driver.java
+++ Driver.java
@@ -21,8 +21,8 @@ public class Driver {
         System.out.println("Enter your Name!");
         String userName = input.nextLine();
 
-        //makeStory();
-        testStory();
+        makeStory();
+        //testStory();
 
         player = new Player(userName, 30, startRoom);
 
@@ -40,7 +40,7 @@ public class Driver {
         while(true) {
             Room currRoom = player.getRoom();
             if(player.timeIsUp()) {
-                System.out.println("Game Over!");
+                System.out.println("Game Over! \n You took too long to fix the purifier. Your vision slowly fades to black as you suffocate. You wake up in New Jersey five hours later. You should probably lay off the benadryl for a while");
                 break;
             }
 
@@ -71,44 +71,18 @@ public class Driver {
             System.out.println("You lost " + player.getName() + "!");
     }
 
-    private static void testStory() {
-        rooms = new ArrayList<>();
-        Room testroom = new Room("Testroom", "This is an example test room");
-        rooms.add(testroom);
-        startRoom = testroom;
-
-        testroom.addOption(new Option("Take item", "you took the item", 5));
-        testroom.lastSetItems("item1");
-        testroom.lastSetFlags("this is a flag");
-
-        testroom.addOption(new Option("Use item", "you used the item"));
-        testroom.lastNeedItems("item1");
-        testroom.lastSetFlags("gameEnd");
-
-    }
-
     /**
      * Sets up the game story by creating rooms and their respective options.
      */
     private static void makeStory() {//example
+        System.out.println("You wake up on your spaceship to a blaring alarm repeating \"the air purification system is damaged. Proceed to the purification room to do further diagnostics.\"");
         rooms = new ArrayList<>();
-        Room bedroom = new Room("Bedroom", 
-        "You wake up on your spaceship to a blaring alarm repeating
-        \“the air purification system is damaged. Proceed to the purification room to do further diagnostics.\”.   
-        You look around the room and see [1] a door straight ahead,  [2] a closet to the right, [3] an alarm clock to the left, 
-        and [4] an old missile on the wall");
-        bedroom.addOption(new Option("You inspect the missile and notice that it is still live and functional, would you like to take it?"
-        , "You take the missile somehow carrying it."));
-        
-
-
-
-        Room closet = new Room("Closet", "closet (hammer)");
-        Room hallway = new Room("Hallway", "hallway stuff");
-        Room storage = new Room("Storage", "storage stuff");
-        Room purifier = new Room("Purifier", "purifier stuff");
-        Room controlRoom = new Room("Control Room", "control stuff");
-
+        Room bedroom = new Room("Bedroom", "You look around your bedroom and see,");
+        Room closet = new Room("Closet", "You look in the closet and see,");
+        Room hallway = new Room("Hallway", "You walk into the hallway and see red lights flashing,");
+        Room storage = new Room("Storage", "You are at the storage room,");
+        Room purifier = new Room("Purifier", "You see the air purifier straight ahead,");
+        Room controlRoom = new Room("Control Room", "You enter the main control room and see,");
         rooms.add(bedroom);
         rooms.add(closet);
         rooms.add(hallway);
@@ -117,6 +91,110 @@ public class Driver {
         rooms.add(controlRoom);
         startRoom = bedroom;
 
+
+        //Bedroom choices, and closet
+        bedroom.addOption(new Option("Go back to sleep. You're sure the problem will sort itself out.", 
+        "You die, in your sleep slowly suffocating.", 100));
+        bedroom.lastSetFlags("gameEnd");
+
+        bedroom.addOption(new Option("You inspect the missile and notice that it is still live and functional.",
+        "You take the missile somehow carrying it."));
+        bedroom.lastSetItems("missle");
+
+        bedroom.addOption(new Option("You see a door leading to the closet.", 
+        "You enter the closet.", 2, closet));
+
+        closet.addOption(new Option("You see a sledgehammer you could take it.", 
+        "You pick up the sledgehammer.", 2));
+        closet.lastSetItems("sledgehammer");
+        closet.addOption(new Option("Get dressed.", "You look extraordinarily dapper.", -8));
+        closet.addOption(new Option("Leave closet.", "You leave the closet.", 1, bedroom));
+
+        bedroom.addOption(new Option("You see an alarm clock.", "It looks like you could break this."));
+        bedroom.lastSetFlags("alarmLook");
+        bedroom.addOption(new Option("You see an alarm clock that looks very breakable.", 
+        "You smash the alarm clock with your sledgehammer and some batteries fall out.", 3));
+        bedroom.lastNeedFlags("alarmLook");
+        bedroom.lastNeedItems("sledgehammer");
+        bedroom.lastSetItems("batteries");
+
+        bedroom.addOption(new Option("You see a door leading to the hallway", 
+        "You leave the bedroom.", 2, hallway));
+
+
+        //Hallway choices
+        hallway.addOption(new Option("You see a broken door leading to the storage room.", 
+        "The door does not seem to work.", 1, storage));
+
+        hallway.addOption(new Option("You see a door leading to the air purifier room.", 
+        "You enter the air purifier room.", 2, purifier));
+
+        hallway.addOption(new Option("You see a door leading to the main control room", 
+        "You enter the main control room.", 2, controlRoom));
+
+        //Control Room choices
+        controlRoom.addOption(new Option("You see a console with a warning message on it.", 
+        "The warning message on the console reads “Proceed to the air purifier room and remove the front panel. Internal rewiring is required”."));
+
+        controlRoom.addOption(new Option("You see a keycard hanging on the wall.", 
+        "You take the keycard."));
+        controlRoom.lastSetItems("keycard");
+
+        controlRoom.addOption(new Option("You see a new airpurifier.", 
+        "You pick it up but its very heavy.", 5));
+        controlRoom.lastNeedItems("airpurifier");
+
+        controlRoom.addOption(new Option("Leave the control room.", 
+        "You go back to the hallway.", 1, hallway));
+
+
+        //Storage
+        storage.addOption(new Option("You could blow up the door with the missle.", 
+        "You blow up the door but also put a hole in the side of your spaceship. You suffocate and die."));
+        storage.lastNeedItems("missle");
+        storage.lastSetFlags("gameEnd");
+
+        storage.addOption(new Option("You could replace the keycard readers batteries.", 
+        "You replace the batteries in the keycard reader and it turns on."));
+        storage.lastNeedItems("batteries");
+        storage.lastSetFlags("keycardReaderOn");
+
+        storage.addOption(new Option("You could use the keycard.", 
+        "You use the keycard and the storage room door opens."));
+        storage.lastNeedFlags("keycardReaderOn");
+        storage.lastNeedItems("keycard");
+        storage.lastSetFlags("doorOpen");
+
+        storage.addOption(new Option("You see a toolbox in the storage room.", 
+        "You take the toolbox."));
+        storage.lastNeedFlags("doorOpen");
+        storage.lastSetItems("toolbox");
+
+        //puzzle
+        storage.addOption(new Option("Would you like to guess the password?", 
+        "You somehow guess the password to the door."));
+        storage.lastNeedFlags("keycardReaderOn");
+        storage.lastSetFlags("doorOpen");
+
+        storage.addOption(new Option("Leave storage room.", 
+        "You go back to the hallway.", 1, hallway));
+
+        //Air Purifier
+        purifier.addOption(new Option("Leave air purifier room.", 
+        "You go back to the hallway.", 3, hallway));
+
+        purifier.addOption(new Option("Look at the purifier.", 
+        "You take a look and you can't fix it without a toolbox or new purifer."));
+
+        purifier.addOption(new Option("You use the screwdriver in your toolbox to take the front panel from the purifier.",
+         "You fix the air purifier! \n Congratulations! You successfully fixed the air purifier in time and didn’t die!"));
+        purifier.lastNeedItems("toolbox");
+        purifier.lastSetFlags("gameEnd");
+
+        purifier.addOption(new Option("You could try replacing the whole air purifier but it would be hard.", 
+        "You somehow install the new air purifier! \n Congratulations! You successfully fixed the air purifier in time and didn’t die!"));
+        purifier.lastNeedItems("airpurifier");
+        purifier.lastSetFlags("gameEnd");
 
     }
 
